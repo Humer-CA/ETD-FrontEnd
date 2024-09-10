f8f94cd932d6a4a5f9bb14eb31f428d24b345d44
humer-CA <humeraltares@gmail.com>
2024-09-10 09:02:45 +0800

    working on the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,93 +1,111 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import etdLogo from "../../public/Images/ETD.svg";
 import heroImg from "../../public/Images/Hero Img1.jpg";
-import Link from "next/link";
-import { Input } from "@nextui-org/input";
-import { Button } from "@nextui-org/react";
-import { redirect, useRouter } from "next/navigation";
-import { LuMenu, LuSearch } from "react-icons/lu";
+import { Button, Divider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { LuSearch } from "react-icons/lu";
+import { FaRegFileLines, FaSolarPanel } from "react-icons/fa6";
+import { MdElectricBolt, MdPayments } from "react-icons/md";
+import Navbar from "../Components/Navbar";
+import CardSimple from "../Components/CardSimple";
 
 const Dashboard = () => {
-  const router = useRouter();
+  const [isLg, setIsLg] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLg(window.innerWidth >= 1024);
+    };
+
+    // Set initial value
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log(isLg);
   return (
     <div>
-      <nav className="flex items-center justify-around gap-4">
-        <Image src={etdLogo} alt="logo" className="object-contain w-32 py-2" />
-        <div className="flex items-center justify-center gap-20 sm:hidden md:flex ">
-          <ul className="flex gap-8">
-            <Link href="/" className="">
-              Home
-            </Link>
-            <Link href="/category" className="">
-              Category
-            </Link>
-            <Link href="/pricing" className="">
-              Pricing
-            </Link>
-            <Link href="/inquiry" className="">
-              Inquire
-            </Link>
-            <Link href="/about" className="">
-              About
-            </Link>
-            <Link href="/contact" className="">
-              Contact
-            </Link>
-          </ul>
+      <Navbar />
 
-          <div className="flex items-center gap-4">
-            <Input
-              type="text"
-              variant="flat"
-              radius="lg"
-              startContent={<LuMenu size={30} className="bg-transparent" />}
-            />
-            <Button
-              onClick={() => router.push("/login")}
-              radius="lg"
-              className="bg-gradient-to-tr from-primaryLight to-primaryDark text-white shadow-lg"
-            >
-              Sign In
-            </Button>
-          </div>
-        </div>
-
-        <Button className="sm:flex md:hidden" isIconOnly>
-          <LuMenu size={30} className="bg-transparent" />
-        </Button>
-      </nav>
-
-      <section className="relative h-full">
+      <section className="relative flex flex-col h-full gap-10">
         <div className="flex flex-col justify-center items-center">
-          <div className="absolute inset-0 flex items-center justify-center m-auto">
-            <div className="flex flex-col items-center justify-center max-w-[1200px] gap-5 text-lightgray">
+          <div className="absolute flex items-center justify-center m-auto">
+            <div className="flex flex-col items-center justify-center max-w-[1200px] gap-5 text-slate-200">
               <p className="font-bold text-lg">ETD SOLAR</p>
-              <p className="font-bold text-5xl text-white">Get Solar Powered Now</p>
-              <p className=" text-center">
+              <p className="font-bold text-3xl lg:text-6xl md:text-4xl sm:text-3xl xs:text-2xl text-white">
+                Get Solar Powered Now
+              </p>
+              <p className="p-5 px-24 text-center">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam hendrerit accumsan enim vitae lobortis.
                 Morbi tempor, tellus quis bibendum ornare, nulla nunc pulvinar mi, a consequat dolor orci sed erat.
               </p>
 
-              <div>
-                <Button variant="shadow" color="primary" size="lg">
+              <div className="flex flex-col gap-6 justify-center items-center px-10">
+                <Button color="primary" size="lg" className="w-40">
                   Learn More
                 </Button>
+                <div className="flex flex-col lg:flex-row items-center justify-center bg-slate-50 rounded-xl p-5 gap-6 w-[250px] lg:w-[auto]">
+                  <ul className="flex flex-col lg:flex-row gap-4 h-auto items-center justify-center text-slate-800 lg:h-12 [&>li>div]:flex lg:[&>li>div]:flex-row [&>li>div]:items-center [&>li>div]:gap-2 [&>li>p]:text-slate-500 [&>li>p]:text-sm">
+                    <li>
+                      <div>
+                        <FaSolarPanel className="text-slate-500" />
+                        <span>Solar Panels</span>
+                      </div>
+                      <p>Lorem ipsum odor amet.</p>
+                    </li>
+                    <Divider orientation={!isLg ? "horizontal" : "vertical"} />
+                    <li>
+                      <div>
+                        <MdElectricBolt className="text-slate-500" />
+                        <span>Inverter</span>
+                      </div>
+                      <p>Lorem ipsum odor amet.</p>
+                    </li>
+                    <Divider orientation={!isLg ? "horizontal" : "vertical"} />
+                    <li>
+                      <div>
+                        <MdPayments className="text-slate-500" />
+                        <span>Pricing</span>
+                      </div>
+                      <p>Lorem ipsum odor amet.</p>
+                    </li>
+                    <Divider orientation={!isLg ? "horizontal" : "vertical"} />
+                    <li>
+                      <div>
+                        <FaRegFileLines className="text-slate-500" />
+                        <span>Application</span>
+                      </div>
+                      <p>Lorem ipsum odor amet.</p>
+                    </li>
+                  </ul>
+                  <Button color="primary" startContent={<LuSearch size={16} />} className="rounded-lg">
+                    Search
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
-          <div className="bg-slate-500 z-[-1] flex max-h-[950px]">
+          <div className="bg-slate-500 z-[-1] flex h-[950px] lg:h-[750px]">
             <Image src={heroImg} alt="logo" className="object-cover object-bottom mix-blend-multiply" />
           </div>
         </div>
-      </section>
 
-      <div></div>
-      <div></div>
-      <div></div>
+        <div className="flex flex-col items-center justify-center">
+          <h3 className="font-bold text-3xl">Our Engineering Services</h3>
+          <p className="p-5 px-24 text-center text-slate-500">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam hendrerit accumsan enim vitae lobortis. Morbi
+            tempor, tellus quis bibendum ornare.
+          </p>
+          <CardSimple />
+        </div>
+      </section>
     </div>
   );
 };
